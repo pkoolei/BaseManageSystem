ebbae606c3714ddf498e1d9ceb548420c52021fc
18620010615 <1638465127@qq.com>
2019-11-11 17:20:13 +0800

    2011-11-11 地图页面和路由配置 ---钟松林

--- BaseManageSystem_front_end/vue-element-admin/src/router/index.js
+++ BaseManageSystem_front_end/vue-element-admin/src/router/index.js
@@ -12,6 +12,7 @@ import chartsRouter from './modules/charts'
 import tableRouter from './modules/table'
 import nestedRouter from './modules/nested'
 import kpiRouter from './modules/kpi'
+import mapRouter from './modules/map'
 /**
  * Note: sub-menu only appear when route children.length >= 1
  * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
@@ -190,7 +191,7 @@ export const asyncRoutes = [
   chartsRouter,
   nestedRouter,
   tableRouter,
-
+  mapRouter,
 
   {
     path: '/example',
