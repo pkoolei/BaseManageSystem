d4cefc264554be95b67b454e01c8676fb73d28ce
pkoolei <76603842@qq.com>
2019-11-12 16:16:30 +0800

    KPI模块：完成MOCK数据模拟，完成筛选查询逻辑，完成流量图显示

--- BaseManageSystem_front_end/vue-element-admin/mock/index.js
+++ BaseManageSystem_front_end/vue-element-admin/mock/index.js
@@ -5,12 +5,14 @@ import user from './user'
 import role from './role'
 import article from './article'
 import search from './remote-search'
+import kpi from './kpi'
 
 const mocks = [
   ...user,
   ...role,
   ...article,
-  ...search
+  ...search,
+  ...kpi
 ]
 
 // for front mock
