49be8e89389aac0df30e6160d9393a870d682a0e
lostdeer96 <316200901>
2019-11-13 09:12:29 +0800

    add tousu

--- BaseManageSystem_front_end/vue-element-admin/src/router/modules/tousu.js
+++ BaseManageSystem_front_end/vue-element-admin/src/router/modules/tousu.js
@@ -7,24 +7,24 @@ const tousuRouter = {
   component: Layout,
   redirect: 'noRedirect',
   name: 'tousugongdan',
-  meta: {
-    title: '投诉工单', // g
+    meta: {
+    title: '投诉工单',//g
     icon: 'table'
   },
   children: [
     {
       path: 'AddTousu',
-      component: () => import('@/views/tousu/AddTousu'), // 路径
-      name: 'AddTousu',
-      meta: { title: 'Add Tousu', noCache: true }
-    },
-    {
-      path: 'ShowTousu',
-      component: () => import('@/views/tousu/ShowTousu'),
-      name: 'ShowTousu',
-      meta: { title: 'Show Tousu', noCache: true }
-    }
-  ]
+      component: () => import('@/views/tousu/AddTousu'),//路径
+    name: 'AddTousu',
+  meta: { title: 'Add Tousu', noCache: true }
+},
+{
+  path: 'ShowTousu',
+    component: () => import('@/views/tousu/ShowTousu'),
+  name: 'ShowTousu',
+  meta: { title: 'Show Tousu', noCache: true }
+}
+]
 }
 
 export default tousuRouter
