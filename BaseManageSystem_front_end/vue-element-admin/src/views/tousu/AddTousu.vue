48e31deaa7049dc028adb06248423a2911512396
lostdeer96 <316200901>
2019-11-16 09:32:46 +0800

    样式调整

--- BaseManageSystem_front_end/vue-element-admin/src/views/tousu/AddTousu.vue
+++ BaseManageSystem_front_end/vue-element-admin/src/views/tousu/AddTousu.vue
@@ -1,42 +1,37 @@
 <template>
-  <div>
-    <el-form ref="form" :model="form" label-width="80px">
+  <div id="form">
+    <el-form ref="form" :model="form" label-width="100px">
 
       <el-form-item label="派单时间">
-        <el-col :span="11">
+        <el-col :span="4">
           <el-date-picker v-model="form.sendtime" type="date" placeholder="选择日期" style="width: 100%;" />
         </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
+        <el-col class="line" :span="1">-</el-col>
+        <el-col :span="4">
           <el-time-picker v-model="form.date2" type="fixed-time" placeholder="选择时间" style="width: 100%;" />
         </el-col>
       </el-form-item>
 
-      <el-form-item label="投诉号码">
-        <el-input v-model="form.phonenumber" />
-      </el-form-item>
-
-      <el-form-item label="客服要求回复时限">
-        <el-col :span="11">
+      <el-form-item label="要求时限">
+        <el-col :span="4">
           <el-date-picker v-model="form.deadline" type="date" placeholder="选择日期" style="width: 100%;" />
         </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
+        <el-col class="line" :span="1">-</el-col>
+        <el-col :span="4">
           <el-time-picker v-model="form.date2" type="fixed-time" placeholder="选择时间" style="width: 100%;" />
         </el-col>
       </el-form-item>
 
+      <el-row :gutter="20">
+        <el-col :span="6">
       <el-form-item label="网络类型">
         <el-select v-model="form.nettype" placeholder="请选择网络类型">
           <el-option label="LTE" value="LTE" />
           <el-option label="WCDMA" value="WCDMA" />
         </el-select>
       </el-form-item>
-
-      <el-form-item label="工单编号">
-        <el-input v-model="form.WOnumber" />
-      </el-form-item>
-
+          </el-col>
+        <el-col :span="6">
       <el-form-item label="处理状态">
         <el-select v-model="form.status" placeholder="请选择活动区域">
           <el-option label="待处理" value="shanghai" />
@@ -44,33 +39,57 @@
           <el-option label="已完成" value="beijing" />
         </el-select>
       </el-form-item>
+        </el-col>
+      </el-row>
 
-      <el-form-item label="工单主题">
-        <el-input v-model="form.WOtheme" />
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <el-form-item label="责任人">
+            <el-input v-model="form.responsible" style="width:200px" />
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="6">
+          <el-form-item label="责任人电话">
+            <el-input v-model="form.responnumber" style="width:250px"/>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row :gutter="20">
+        <el-col :span="6">
+      <el-form-item label="投诉号码">
+        <el-input style="width:250px" v-model="form.phonenumber"  />
       </el-form-item>
+        </el-col>
+      <el-col :span="6">
+      <el-form-item label="工单编号">
+        <el-input style="width:250px" v-model="form.WOnumber" />
+      </el-form-item>
+      </el-col>
+        <el-col :span="6">
+          <el-form-item label="工单主题" >
+            <el-input  v-model="form.WOtheme"  style="width:250px"/>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
 
       <el-form-item label="投诉内容">
-        <el-input v-model="form.complaint" />
+        <el-input type="textarea" v-model="form.complaint" style="width:700px"/>
       </el-form-item>
 
       <el-form-item label="备注">
-        <el-input v-model="form.remarks" />
+        <el-input type="textarea" v-model="form.remarks" style="width:700px"/>
       </el-form-item>
 
-      <el-form-item label="责任人">
-        <el-input v-model="form.responsible" />
-      </el-form-item>
-
-      <el-form-item label="责任人电话">
-        <el-input v-model="form.responnumber" />
-      </el-form-item>
 
       <el-form-item label="问题原因">
-        <el-input v-model="form.reason" />
+        <el-input type="textarea" v-model="form.reason" style="width:700px"/>
       </el-form-item>
 
       <el-form-item label="解决方案">
-        <el-input v-model="form.solution" />
+        <el-input type="textarea" v-model="form.solution" style="width:700px"/>
       </el-form-item>
 
       <el-form-item>
@@ -121,4 +140,8 @@ export default {
 body{
   background-color: darkgrey;
 }
+  #form{
+    margin-top: 50px;
+  }
+
 </style>
